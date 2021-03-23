4d6f090fdf10a3586d8ee5c42251e565f29c0863
Ashish <ashcanbe7@gmil.com>
2021-03-23 17:46:12 +0530

    final

--- My-Dummy-Proj/src/Hello.java
+++ My-Dummy-Proj/src/Hello.java
@@ -3,12 +3,10 @@ public class Hello {
 	
 	public void method1() {
 		System.out.println("method1");
-<<<<<<< HEAD
 
-=======
 		System.out.println("hi");
 		System.out.println("hii");
->>>>>>> branch 'master' of https://github.com/Ashish7666/fictional-guide.git
+		
 	System.out.println("hello");
 	
 	
